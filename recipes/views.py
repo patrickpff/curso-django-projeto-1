d779a9a8f28bfdc060af731bb9654837094d43e0
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2024-02-06 11:50:29 -0300

    remoção de códigos

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home (request):
     return render(request=request, template_name='recipes/home.html', context={
         'name': "Patrick"
     })
-
-def contact(request):
-    return HttpResponse("HELLO WORLD - CONTATO")
-
-def about(request):
-    return HttpResponse("HELLO WORLD - SOBRE")

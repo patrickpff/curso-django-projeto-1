9aa22c8d4b6fdfb2f18fba0e5d9e25d2c9e3fc97
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2024-02-07 17:01:18 -0300

    adequação dos links da página  de receitas e de lista

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = "recipe"
+
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe)
+    path('', views.home, name="list"),
+    path('recipes/<int:id>/', views.recipe, name="read")
 ]

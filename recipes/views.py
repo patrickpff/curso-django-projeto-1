00060eae658a86609f1d9725c44157ec252b52ad
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2024-02-07 16:40:37 -0300

    aula 48

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 def home (request):
     return render(request=request, template_name='recipes/pages/home.html', context={
-        'name': "Patrick Ferreira"
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
-def recipe(request, id):
-    return render(request=request, template_name='recipes/pages/home.html', context={
-        'name': 'Patrick Ferrera'
+def recipe (request, id):
+    return render(request=request, template_name='recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })

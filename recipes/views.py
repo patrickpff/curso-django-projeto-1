3103e461ab34a1b7ca90ac2a50c9ef2d0809479c
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2024-02-08 18:14:45 -0300

    adicionado get_list_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
 from django.http import Http404
 
@@ -11,14 +11,16 @@ def home (request):
     })
 
 def category (request, id):
-    recipes = Recipe.objects.filter(category__id=id, is_published=True).order_by('-id')
-
-    if not recipes:
-        raise Http404('Not Found :(')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=id, 
+            is_published=True
+        ).order_by('-id')
+    )
 
     return render(request=request, template_name='recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes.first().category.name}  - Category |'
+        'title': f'{recipes[0].category.name}  - Category |'
     })
 
 def recipe (request, id):

9c52dc4e9fbfcf50e94c8c4df9c65d291a86d5fb
Patrick Pierre Fernandes Ferreira <patrickpierreff@gmail.com>
2024-02-08 11:26:09 -0300

    configuracao do salvamento de arquivos de media

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category
+from .models import Recipe
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Category, CategoryAdmin)
+
--- recipes/models.py
+++ recipes/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
